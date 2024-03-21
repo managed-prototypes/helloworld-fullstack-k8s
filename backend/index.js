86f3aecd689585c2d518350b5cbe31d4effd51ce
VladimirLogachev <vladimir@logachev.dev>
2024-03-22 00:17:42 +0400

    Add nginx

--- backend/index.js
+++ backend/index.js
@@ -5,7 +5,7 @@ const Hapi = require("@hapi/hapi");
 const init = async () => {
   const server = Hapi.server({
     port: 8000,
-    host: "localhost",
+    host: "0.0.0.0",
   });
 
   server.route({
